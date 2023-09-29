f45fda9dc2b4e0a4cead2ea451c63a93300e1ad2
FJ <fj@FJs-MacBook-Pro-2.local>
2023-09-29 08:46:31 -0500

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="intro">
       <h1>Nuxt Toolbox</h1>
       <p>
-        Hi 👋! This template gives you a
+        Fort Raven
         <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
         <a href="https://www.netlify.com/products/functions/"
           >Netlify Functions</a
